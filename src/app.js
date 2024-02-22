6d302b4c85f7278849415198275125f5a54f7d0a
felicity <bogueyerif@gmail.com>
2024-02-22 21:40:49 +0100

    modification

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan=require('morgan');
-import bodyparser from 'body-parser'
+const morgan = require('morgan');
+const bodypaser=require('body-parser')
 
 
 //const app = express=();
 
-app.use(morgan('dev'));
+app.use(morgan ('dev'));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({exports:false}))
