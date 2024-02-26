8588d51797a43a5161c4b3f979fc3a61d8fdf22c
felicity <bogueyerif@gmail.com>
2024-02-26 16:12:31 +0100

    ajouter les modification

--- src/routes/fournisseur.route.js
+++ src/routes/fournisseur.route.js
@@ -1,10 +1,19 @@
-const express=require('express');
+import express from 'express';
+
+import *as FournisseurCtrl  from '../controllers/fournisseur.controller'
 
 
-export *as FournisseurCtl from "../controllers/fournisseur.controller" ;
+const fournisseurRouter = express.Router();
+
+fournisseurRouter.post('/login', FournisseurCtrl.login);
+fournisseurRouter.post('/add-fournisseur', FournisseurCtrl.addFournisseur);
+
+
+import *as FournisseurCtl from "../controllers/fournisseur.controller" ;
 
 
 
 fourniseurRouter.post('/add-fournisseur', FournisseurCtl.addfournisseur);
 
+module.exports = fournisseurRouter;
 
